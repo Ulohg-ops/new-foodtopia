e9554e215a1dbb0b493398fb7cdd0c09b01546aa
hsu27 <nina124215545@gmail.com>
2022-02-08 10:00:26 +0800

    加bottom navigation

--- app/src/main/java/com/example/foodtopia/MainActivity.java
+++ app/src/main/java/com/example/foodtopia/MainActivity.java
@@ -1,36 +1,65 @@
 package com.example.foodtopia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    Fragment fragment = new Dashboard();
 
-    private Button btn;
+//    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //顯示首頁
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment).commit();
+
+
+        //點選下方工具列切換頁面
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, introductoryActivity.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.Dashboard:
+                        fragment = new Dashboard();
+                        break;
+                    case R.id.Restaurant:
+                        fragment = new Restaurant();
+                        break;
+                    case R.id.Add:
+                        fragment = new Add();
+                        break;
+                    case R.id.Social:
+                        fragment = new Social();
+                        break;
+                    case R.id.Account:
+                        fragment = new Account();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment).commit();
+                return true;
             }
         });
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello world!");
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//
+//        myRef.setValue("Hello world!");
     }
 }
