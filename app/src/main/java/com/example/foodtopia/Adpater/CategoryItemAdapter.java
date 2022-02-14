90ceb7dcbedd9b0e3f031cc9a5b4c52416959f03
ulohg <gg.gg5241@gmail.com>
2022-02-14 19:03:32 +0800

    add data to firestroe

--- app/src/main/java/com/example/foodtopia/Adpater/CategoryItemAdapter.java
+++ app/src/main/java/com/example/foodtopia/Adpater/CategoryItemAdapter.java
@@ -3,31 +3,46 @@ package com.example.foodtopia.Adpater;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodtopia.R;
 import com.example.foodtopia.model.CategoryItem;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CategoryItemAdapter extends RecyclerView.Adapter<CategoryItemAdapter.CategoryItemViewHolder> {
 
     private Context context;
     private List<CategoryItem> categoryItemList;
+    final String TAG = "MyActivity";
 
-    private AlertDialog.Builder dialogBuilder;
-    private AlertDialog dialog;
-
+    Date dNow ;
+    SimpleDateFormat ft =
+            new SimpleDateFormat("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public CategoryItemAdapter(Context context, List<CategoryItem> categoryItemList) {
         this.context = context;
@@ -42,16 +57,17 @@ public class CategoryItemAdapter extends RecyclerView.Adapter<CategoryItemAdapte
 
     @Override
     public void onBindViewHolder(@NonNull CategoryItemViewHolder holder, int position) {
-        String foodName2=categoryItemList.get(position).getFoodName();
-        String calories2=categoryItemList.get(position).getCalories();
-        String carbohydrate2=categoryItemList.get(position).getCarbohydrate();
-        String fat2=categoryItemList.get(position).getFat();
-        String protein2=categoryItemList.get(position).getProtein();
-        String saturatedfat2=categoryItemList.get(position).getSaturatedfat();
-        String unsaturatedfat2=categoryItemList.get(position).getUnsaturatedfat();
-        String sodium2=categoryItemList.get(position).getSodium();
-        String sugars2=categoryItemList.get(position).getSugars();
+        String foodName2 = categoryItemList.get(position).getFoodName();
+        String calories2 = categoryItemList.get(position).getCalories();
+        String carbohydrate2 = categoryItemList.get(position).getCarbohydrate();
+        String fat2 = categoryItemList.get(position).getFat();
+        String protein2 = categoryItemList.get(position).getProtein();
+        String saturatedfat2 = categoryItemList.get(position).getSaturatedfat();
+        String unsaturatedfat2 = categoryItemList.get(position).getUnsaturatedfat();
+        String sodium2 = categoryItemList.get(position).getSodium();
+        String sugars2 = categoryItemList.get(position).getSugars();
 
+        String meal;
         holder.itemfood.setText(categoryItemList.get(position).getFoodName());
         holder.arrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,12 +76,12 @@ public class CategoryItemAdapter extends RecyclerView.Adapter<CategoryItemAdapte
                 View diaglogView = LayoutInflater.from(view.getRootView().getContext()).inflate(R.layout.restaurant_popup_window, null);
 
                 Spinner spinner = diaglogView.findViewById(R.id.spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
+                // Create an ArrayAdapter using the string array and a default spinner layout
                 ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context,
                         R.array.meal_time, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+                // Specify the layout to use when the list of choices appears
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+                // Apply the adapter to the spinner
                 spinner.setAdapter(adapter);
 
                 FloatingActionButton add;
@@ -79,15 +95,7 @@ public class CategoryItemAdapter extends RecyclerView.Adapter<CategoryItemAdapte
                 TextView sodium;
                 TextView sugars;
 
-                add=diaglogView.findViewById(R.id.add);
-                add.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        System.out.println("ahoy");
-                    }
-                });
-
-                calories=diaglogView.findViewById(R.id.calories);
+                calories = diaglogView.findViewById(R.id.calories);
                 calories.setText(calories2);
 
                 foodName = diaglogView.findViewById(R.id.foodName);
@@ -114,6 +122,17 @@ public class CategoryItemAdapter extends RecyclerView.Adapter<CategoryItemAdapte
                 sugars = diaglogView.findViewById(R.id.sugars);
                 sugars.setText(sugars2);
 
+                add = diaglogView.findViewById(R.id.add);
+                add.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String mealtime=spinner.getSelectedItem().toString();
+                        addMymeal(foodName2, calories2, carbohydrate2, fat2,
+                                protein2, saturatedfat2, unsaturatedfat2, sodium2, sugars2, mealtime);
+                    }
+                });
+
+
                 builder.setView(diaglogView);
                 builder.setCancelable(true);
                 builder.show();
@@ -139,11 +158,39 @@ public class CategoryItemAdapter extends RecyclerView.Adapter<CategoryItemAdapte
         }
     }
 
-    public void createPopUpDialog() {
-        dialog.setContentView(R.layout.restaurant_popup_window);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-//        TextView FoodTitle = dialog.findViewById(R.id.foodName);
-        dialog.show();
+    public void addMymeal(String foodname, String calories, String carbohydrate, String fat, String protein,
+                          String saturatedfat, String unsaturatedfat, String sodium, String sugar, String mealtime) {
+        Map<String, Object> user = new HashMap<>();
+
+        ft = new SimpleDateFormat("yyyyMMdd_hhmmss");
+        dNow = new Date();
+        user.put("foodname", foodname);
+        user.put("calories", calories);
+        user.put("carbohydrate", carbohydrate);
+        user.put("fat", fat);
+        user.put("protein", protein);
+        user.put("saturatedfat", saturatedfat);
+        user.put("unsaturatedfat", unsaturatedfat);
+        user.put("sodium", sodium);
+        user.put("sugar", sugar);
+
+
+        db.collection("user_001").document(ft.format(dNow)+"_"+mealtime)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(context, "新增成功!!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+
 
     }
+
 }
