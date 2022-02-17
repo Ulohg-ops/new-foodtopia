f6b5895a9a665117dedeec5795abafe14e043a5b
Ulohg <gg.gg5241@gmail.com>
2022-02-17 16:00:08 +0800

    add email in account

--- app/src/main/java/com/example/foodtopia/Account.java
+++ app/src/main/java/com/example/foodtopia/Account.java
@@ -3,9 +3,11 @@ package com.example.foodtopia;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,42 +17,28 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.foodtopia.classes.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Account#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Account extends Fragment {
-
-    AppCompatButton logout;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Account() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Account.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Account newInstance(String param1, String param2) {
         Account fragment = new Account();
         Bundle args = new Bundle();
@@ -63,10 +51,7 @@ public class Account extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     // account list
@@ -87,7 +72,30 @@ public class Account extends Fragment {
         listView = (ListView) view.findViewById(R.id.profile_list);
 
 
-        //--log out btn
+        //-------------------user email and name----------------------
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
+                .child("Users").child(user.getUid());
+        TextView profile_name=view.findViewById(R.id.profile_name);
+        TextView profile_email=view.findViewById(R.id.profile_email);
+
+    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    mDatabase.child("Users").child("").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        @Override
+        public void onComplete(@NonNull Task<DataSnapshot> task) {
+            if (!task.isSuccessful()) {
+                Log.e("firebase", "Error getting data", task.getException());
+            }
+            else {
+//                Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                System.out.println("dasdasads"+String.valueOf(task.getResult().getValue()));
+            }
+        }
+    });
+        profile_email.setText(user.getEmail());
+//        profile_name.setText(user.getEmail());
+        //-------------------user email and name----------------------
+
+        //----------------------log out btn----------------------------
         //https://firebase.google.com/docs/auth/android/custom-auth
         AppCompatButton logout=view.findViewById(R.id.logoutButton);
         logout.setOnClickListener(new View.OnClickListener() {
@@ -98,7 +106,7 @@ public class Account extends Fragment {
                 startActivity(intent);
             }
         });
-        //--log out btn
+        //----------------------log out btn----------------------------
 
         CustomAdapter customAdapter = new CustomAdapter();
         listView.setAdapter(customAdapter);
