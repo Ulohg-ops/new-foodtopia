2009f3d6e25cc170a558e96bdc10cba079030b97
Jacq <euv0104@gmail.com>
2022-02-18 17:45:32 +0800

    dashboard_circularbar

--- app/src/main/java/com/example/foodtopia/Dashboard.java
+++ app/src/main/java/com/example/foodtopia/Dashboard.java
@@ -3,6 +3,7 @@ package com.example.foodtopia;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.LongDef;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -32,53 +33,26 @@ import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Dashboard#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+
 public class Dashboard extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String TAG = "variable";
 
     public Dashboard() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Dashboard.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Dashboard newInstance(String param1, String param2) {
-        Dashboard fragment = new Dashboard();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
     }
 
     // retrieve data from firebase
@@ -86,43 +60,46 @@ public class Dashboard extends Fragment {
     CircularProgressBar circularProgressBar;
     TextView calories;
 
+    //todo: need to change the path.
+    DocumentReference doc = db.collection("test_user").document("fXnfzXMPoQl4fmzqcNVK");
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-//        circularProgressBar = view.findViewById(R.id.circularProgressBar);
-//        circularProgressBar.setProgressWithAnimation(65f);
-//        circularProgressBar.setProgressMax(200f);
+        circularProgressBar = view.findViewById(R.id.circularProgressBar);
+        calories = view.findViewById(R.id.calories);
 
+        doc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String calories_per_day = (String) document.get("calories_per_day");
+                        String calories_taken = (String) document.get("calories_taken");
+                        float cpd = Float.valueOf(calories_per_day);
+                        float ct = Float.valueOf(calories_taken);
+
+                        //set circular progress bar
+                        circularProgressBar.setProgress(ct);
+                        circularProgressBar.setProgressMax(cpd);
+
+                        //set text inside the circular
+                        String text = calories_taken + "\n" + "kcal";
+                        calories.setText(text);
+                    } else {
+                        Log.d(TAG, "No doc.");
+                    }
+                }else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
 
         return view;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-//        String currentId = "NR1otdnIRzbaaspHs9CY";
-//        DocumentReference reference;
-//        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-//
-//        reference = firestore.collection("user").document(currentId);
-//
-//        reference.get()
-//                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                        if(task.isSuccessful()){
-//
-//                            circularProgressBar.setProgressWithAnimation(50f);
-////                            circularProgressBar.setProgressMax(daily_f);
-//                        }
-////                        else {
-////
-////                        }
-//                    }
-//                });
-    }
 }
