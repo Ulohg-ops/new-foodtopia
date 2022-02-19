ce6b37930e1478f38a528fd7565fd265941f6103
ulohg <gg.gg5241@gmail.com>
2022-02-19 21:46:35 +0800

    button version

--- app/src/main/java/com/example/foodtopia/Saved_item_Activity.java
+++ app/src/main/java/com/example/foodtopia/Saved_item_Activity.java
@@ -1,22 +1,80 @@
 package com.example.foodtopia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.foodtopia.Adpater.PostAdapter;
+import com.example.foodtopia.Model.Post;
+import com.example.foodtopia.Model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Saved_item_Activity extends AppCompatActivity {
-    //    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    private RecyclerView recyclerView;
+    private PostAdapter postAdapter;
+    private List<Post> postList;
+    String post_id ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_item);
-//         String a= prefs.getString("postid","");
-//        System.out.println(a);
+
         Intent intent = getIntent();
-        String id = intent.getStringExtra("postid");
-        System.out.println(id);
+        post_id = intent.getStringExtra("postid");
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager.setReverseLayout(true);
+        mLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(mLayoutManager);
+        postList = new ArrayList<>();
+        postAdapter = new PostAdapter(this, postList);
+        recyclerView.setAdapter(postAdapter);
+        readPosts();
     }
+    private void readPosts(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                postList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Post post = snapshot.getValue(Post.class);
+                    if(post_id.equals(post.getPostid())){
+                        System.out.println("saddsa");
+                        postList.add(post);
+                        break;
+                    }
+                }
+                postAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
