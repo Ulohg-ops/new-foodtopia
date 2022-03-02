d0394401d552572c5bb120a998bf4e4014b6e814
ulohg <gg.gg5241@gmail.com>
2022-03-03 00:02:29 +0800

    give up

---
+++ app/src/main/java/com/example/foodtopia/BarChartXAxisValueFormatter.java
@@ -0,0 +1,23 @@
+package com.example.foodtopia;
+
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public  class BarChartXAxisValueFormatter extends IndexAxisValueFormatter {
+
+    @Override
+    public String getFormattedValue(float value) {
+
+        // Convert float value to date string
+        // Convert from days back to milliseconds to format time  to show to the user
+        long emissionsMilliSince1970Time = TimeUnit.DAYS.toMillis((long)value);
+        // Show time in local version
+        Date timeMilliseconds = new Date(emissionsMilliSince1970Time);
+        SimpleDateFormat dateTimeFormat = new SimpleDateFormat("MM-dd");
+
+        return dateTimeFormat.format(timeMilliseconds);
+    }
+}
--- app/src/main/java/com/example/foodtopia/CaloriesTrackingActivity.java
+++ app/src/main/java/com/example/foodtopia/CaloriesTrackingActivity.java
@@ -1,14 +1,18 @@
 package com.example.foodtopia;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.bumptech.glide.Glide;
+import com.example.foodtopia.Model.User;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
@@ -23,12 +27,14 @@ import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.ChartTouchListener;
 import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.github.mikephil.charting.utils.MPPointD;
 import com.github.mikephil.charting.utils.Transformer;
 import com.github.mikephil.charting.utils.Utils;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,7 +43,11 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class CaloriesTrackingActivity extends AppCompatActivity {
 
@@ -46,6 +56,7 @@ public class CaloriesTrackingActivity extends AppCompatActivity {
     FirebaseAuth fAuth;
     DatabaseReference reference;
     Query allRecordFromUser;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     private final String TAG = "test";
 
@@ -67,25 +78,135 @@ public class CaloriesTrackingActivity extends AppCompatActivity {
         // chart
         chart = findViewById(R.id.chart);
 
-        // get user id
-        fAuth = FirebaseAuth.getInstance();
-        FirebaseUser firebaseUser = fAuth.getCurrentUser();
-        String userID = firebaseUser.getUid();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        Query query = FirebaseDatabase.getInstance().getReference("Diets")
+                .orderByChild("userid").equalTo(user.getUid());
 
-        reference = FirebaseDatabase.getInstance().getReference("Diets");
-        allRecordFromUser = reference.orderByChild("userid").equalTo(userID);
-
-
-        // get user data
-        allRecordFromUser.addValueEventListener(new ValueEventListener() {
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                float total = 0;
+
+                HashMap<String, List<String>> map = new HashMap<>();
                 for (DataSnapshot record : snapshot.getChildren()) {
-//                    String calories = record.child("calories").getValue(String.class);
-                    float calories = Float.valueOf(record.child("calories").getValue(String.class));
-                    total += calories;
+//                    System.out.println(record.child("date").getValue().toString());
+                    List<String> alternateList = map.get(record.child("date").getValue().toString());
+                    if (alternateList == null) {
+                        alternateList = new ArrayList<>();
+                        map.put(record.child("date").getValue().toString(), alternateList);
+                    }
+                    alternateList.add(record.child("calories").getValue().toString());
                 }
+//                for(Map.Entry<String, List<String>> alternateEntry : map.entrySet()) {
+//                    System.out.println(alternateEntry.getKey() + ": " +
+//                            alternateEntry.getValue().toString());
+//                }
+                HashMap<String, Float> map2 = new HashMap<>();
+                for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+                    String key = entry.getKey();
+                    List<String> value = entry.getValue();
+                    float td_calories_tt = 0;
+                    for (String aString : value) {
+                        System.out.println("key : " + key + " value : " + aString);
+                        td_calories_tt += Float.parseFloat(aString);
+                    }
+                    map2.put(key, td_calories_tt);
+                }
+                Map<String, Float> treeMap = new TreeMap(map2);
+                for (Map.Entry<String, Float> entry : treeMap.entrySet()) {
+                    System.out.println(entry.getKey() + "/" + entry.getValue());
+                }
+//
+//                for (String str : treeMap.keySet()) {
+//                    System.out.println(str);
+//                }
+////
+//                for (Map.Entry<String, Float> entry : map2.entrySet()) {
+//                    System.out.println(entry.getKey() + "/" + entry.getValue());
+//                }
+
+                ArrayList<Float> value = new ArrayList<Float>();
+                ArrayList<BarEntry> entries = new ArrayList<>();
+                String title = "Calories";
+
+
+
+                int a=300;
+                for (Map.Entry<String, Float> entry : treeMap.entrySet()) {
+                        value.add(entry.getValue());
+                        BarEntry barEntry = new BarEntry(a++, entry.getValue());
+                        entries.add(barEntry);
+                }
+                BarDataSet barDataSet = new BarDataSet(entries, title);
+                barDataSet.setColors(ColorTemplate.getHoloBlue());
+                barDataSet.setValueTextColor(Color.BLACK);
+                barDataSet.setValueTextSize(16f);
+
+                BarData data = new BarData(barDataSet);
+                chart.setFitBars(true);
+                chart.setData(data);
+                chart.invalidate();
+                chart.animateY(2000);
+
+
+//                ArrayList<Double> value = new ArrayList<Double>();
+//                ArrayList<BarEntry> entries = new ArrayList<>();
+//                String title = "Title";
+//
+//                for (int i=0; i<6; i++) {
+//                    value.add(i * 100.1);
+//                }
+//
+//                for (int i=0; i<value.size(); i++) {
+//                    BarEntry barEntry = new BarEntry(i+5, value.get(i).floatValue());
+//                    entries.add(barEntry);
+//                }
+//
+//                BarDataSet barDataSet = new BarDataSet(entries, title);
+//                barDataSet.setColors(ColorTemplate.getHoloBlue());
+//                barDataSet.setValueTextColor(Color.BLACK);
+//                barDataSet.setValueTextSize(16f);
+//
+//                BarData data = new BarData(barDataSet);
+//                chart.setFitBars(true);
+//                chart.setData(data);
+//                chart.invalidate();
+//                chart.animateY(2000);
+
+
+                ////---------------ver2
+//                HashMap<String,List<String>> map=new HashMap<>();
+//                for (DataSnapshot record : snapshot.getChildren()) {
+////                    System.out.println(record.child("date").getValue().toString());
+//                    List<String> alternateList = map.get(record.child("date").getValue().toString());
+//                    if(alternateList==null) {
+//                        alternateList=new ArrayList<>();
+//                        map.put(record.child("date").getValue().toString(),alternateList);
+//                    }
+//                    alternateList.add(record.child("calories").getValue().toString());
+//                }
+//                for(Map.Entry<String, List<String>> alternateEntry : map.entrySet()) {
+//                    System.out.println(alternateEntry.getKey() + ": " +
+//                            alternateEntry.getValue().toString());
+//                }
+////---------------ver1
+                //                for (DataSnapshot record : snapshot.getChildren()) {
+//                    System.out.println(record.child("date").getValue().toString());
+//                    List<String> name=new ArrayList<>();
+//                    if(!map.containsKey(record.child("date").getValue().toString())){
+//                        ArrayList<String> name=new ArrayList<>();
+//                        name.add(record.child("calories").getValue().toString());
+//                        map.put(record.child("date").getValue().toString(),name);
+//                    }else{
+//                        map.put(record.child("date").getValue().toString(),
+//                                map.get(record.child("date").getValue().
+//                                toString()));
+//                    }
+//                    //System.out.println(record.child("calories")+" "+record.child("userid")+" "+record.child("userid_date"));;
+//                }
+//                for (Map.Entry<String, ArrayList> entry : map.entrySet()) {
+//                    System.out.println("555");
+//                    System.out.println(entry.getKey() + "/" + entry.getValue());
+//                }
             }
 
             @Override
@@ -94,23 +215,60 @@ public class CaloriesTrackingActivity extends AppCompatActivity {
             }
         });
 
-        ArrayList<Double> value = new ArrayList<Double>();
-        ArrayList<BarEntry> entries = new ArrayList<>();
-        String title = "Title";
 
-        for (int i=0; i<6; i++) {
-            value.add(i * 100.1);
-        }
-
-        for (int i=0; i<value.size(); i++) {
-            BarEntry barEntry = new BarEntry(i, value.get(i).floatValue());
-            entries.add(barEntry);
-        }
-
-        BarDataSet barDataSet = new BarDataSet(entries, title);
-
-        BarData data = new BarData(barDataSet);
-        chart.setData(data);
-        chart.invalidate();
+//        // get user id
+//        fAuth = FirebaseAuth.getInstance();
+//        FirebaseUser firebaseUser = fAuth.getCurrentUser();
+//        String userID = firebaseUser.getUid();
+//
+//        reference = FirebaseDatabase.getInstance().getReference("Diets");
+//        allRecordFromUser = reference.orderByChild("userid").equalTo(userID);
+//
+//
+//
+//
+//        // get user data
+//        allRecordFromUser.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                float total = 0;
+//                for (DataSnapshot record : snapshot.getChildren()) {
+////                    String calories = record.child("calories").getValue(String.class);
+//                    float calories = Float.valueOf(record.child("calories").getValue(String.class));
+//                    total += calories;
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//
+//        ArrayList<Double> value = new ArrayList<Double>();
+//        ArrayList<BarEntry> entries = new ArrayList<>();
+//        String title = "Title";
+//
+//        for (int i=0; i<10; i++) {
+//            value.add(i * 100.1);
+//        }
+//
+//        for (int i=0; i<10; i++) {
+//            BarEntry barEntry = new BarEntry(i, value.get(i).floatValue());
+//            entries.add(barEntry);
+//        }
+//
+//        BarDataSet barDataSet = new BarDataSet(entries, title);
+//        barDataSet.setColors(ColorTemplate.getHoloBlue());
+//        barDataSet.setValueTextColor(Color.BLACK);
+//        barDataSet.setValueTextSize(16f);
+//
+//
+//        BarData data = new BarData(barDataSet);
+//
+//        chart.setFitBars(true);
+//        chart.setData(data);
+//        chart.invalidate();
+//        chart.animateY(2000);
     }
 }
