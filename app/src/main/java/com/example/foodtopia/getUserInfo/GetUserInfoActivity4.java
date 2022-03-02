ac05bb39819c802a1aba3d101665281a999e3a96
ulohg <gg.gg5241@gmail.com>
2022-03-02 14:16:23 +0800

    calc evd calories

--- app/src/main/java/com/example/foodtopia/getUserInfo/GetUserInfoActivity4.java
+++ app/src/main/java/com/example/foodtopia/getUserInfo/GetUserInfoActivity4.java
@@ -50,8 +50,6 @@ public class GetUserInfoActivity4 extends AppCompatActivity {
                 HashMap<String, Object> map = new HashMap<>();
                 map.put("target", radioButton.getText());
                 reference.updateChildren(map);
-
-
                 moveToNext();
             }
         });
